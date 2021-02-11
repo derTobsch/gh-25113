0636b24e3f34c0df4dba3b06808a667f9acf309d
Brian Clozel <bclozel@vmware.com>
2021-02-11 13:33:49 +0100

    Add ResourceUrlEncodingFilter bean
    This is required for JSP setups, see:
    https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-spring-mvc-static-content

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +12,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
+		return new ResourceUrlEncodingFilter();
+	}
 }
