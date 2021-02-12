73eec1d1b07044cdebd5bdee9acace4950323039
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2021-02-12 13:08:06 +0100

    Remove everything except of adding a resource handler
    minimal example

--- src/main/java/com/example/demo/ResourceHandlerConfiguration.java
+++ src/main/java/com/example/demo/ResourceHandlerConfiguration.java
@@ -1,18 +1,15 @@
 package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ResourceHandlerConfiguration extends WebMvcConfigurerAdapter { // NOSONAR
+public class ResourceHandlerConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("/something/**")
-                .addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/META-INF/resources")
-                .resourceChain(false);
+        registry.addResourceHandler("/something/**");
     }
 }
